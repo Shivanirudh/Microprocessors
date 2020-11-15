44d9dfc7c29aed16dbeb70614ca8bd21076d6468
Shivanirudh <shivanirudh2001@gmail.com>
2020-11-15 20:14:14 +0530

    16 bit sum of digits complete

--- Practice/16bitsum51.asm
+++ Practice/16bitsum51.asm
@@ -1,9 +1,27 @@
-#sum of digits in 16-bit number
+;sum of digits in 16-bit number
 
-    mov r0, #12H
-    mov r1, #34H
+        mov r0, #0FFH
+        mov r1, #0FFH
 
-    mov r2, #08H
-    mov r3, #00H
+        mov r2, #08H
+        mov r3, #00H
 
-here: 
+        mov a, r1
+        anl a, #0FH
+        mov r3, a
+        mov a, r1
+        swap a
+        anl a, #0FH
+        add a, r3
+        mov r3, a
+
+        mov a, r0
+        anl a, #0FH
+        add a, r3
+        mov r3, a
+        mov a, r0
+        swap a
+        anl a, #0FH
+        add a, r3
+
+        mov r3, a
