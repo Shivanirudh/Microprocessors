6fde0044e8e573cdf080385a6abcd7ba33dd0df7
Shivanirudh <shivanirudh2001@gmail.com>
2020-11-16 20:43:39 +0530

    Bcd to hex 8051

--- Practice/bcdtohex51.asm
+++ Practice/bcdtohex51.asm
@@ -3,4 +3,20 @@
         mov r0, #34H
 
         mov a, r0
-        
+        mov b, #10h
+
+        anl a, #0f0h
+        div ab
+        mov b, #0Ah
+        mul ab
+        mov r2, a
+        mov r1, b
+
+        mov a, r0
+        anl a, #0fh
+
+        add a, r2
+        addc a, r1
+
+        mov r3, a
+halt:   sjmp halt
